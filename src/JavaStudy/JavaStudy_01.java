d83ea96a86968a57e82847f652503c267e1788bb
jake <jake@laidback.co.kr>
2021-09-12 14:40:10 +0900

    lotto java

--- src/JavaStudy/JavaStudy_01.java
+++ src/JavaStudy/JavaStudy_01.java
@@ -2,6 +2,23 @@ package JavaStudy;
 
 public class JavaStudy_01 {
     public static void main(String[] args) {
-        System.out.println("HELLO JA22VA");
+
+        int[] ball = new int[45];
+
+        for(int i=0; i< ball.length; i++)
+            ball[i] = i+1;
+        int temp;
+        int j;
+
+        for(int i=0; i< 6; i++) {
+            j = (int)(Math.random() * 45); // 0~ 44 범위의 임의의 값을 얻는다
+            temp = ball[i];
+            ball[i] = ball[j];
+            ball[j] = temp;
+        }
+
+        for(int i=0; i<10; i++){
+            System.out.println((i+1)+"번째 로또번호" +  ball[i]);
+        }
     }
 }
