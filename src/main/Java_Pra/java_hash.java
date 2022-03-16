7bca1917a2203962db5167e8ed808012f4a8b6f2
Jakezo <cakim21@naver.com>
2022-03-16 23:15:05 +0900

    2022-03-16 hash study2

--- src/main/Java_Pra/java_hash.java
+++ src/main/Java_Pra/java_hash.java
@@ -7,10 +7,8 @@ import java.io.InputStreamReader;
 public class java_hash {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[] arr = br.readLine().toCharArray();
-
+        char[] arr = br.readLine().toCharArray(); //toCharArray() 메소드는 문자열을 char형 배열로 바꿔준다. 반환되는 배열의 길이는 문자열의 길이와 같다.
         int num=0;
-
         for(int i=0; i<arr.length; i++){
             num += arr[i];
 
