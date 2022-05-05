12e7e86ad163cfcfc813aaf7f07b71a186bc694e
Jakezo <cakim21@naver.com>
2022-05-05 22:21:24 +0900

    소수여부 내풀..

--- src/main/Java_Pra/fibonacciTest.java
+++ src/main/Java_Pra/fibonacciTest.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class fibonacciTest {
-    static long arr[];
+    static long arr[] = new long[1000];
 
     public static long fibonacci2(int i){
         if(i <= 1){
@@ -19,17 +19,18 @@ public class fibonacciTest {
     }
 
     public static void main(String[] args) throws IOException {
-        arr = new long[101];
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         //int n1 = Integer.parseInt(br.readLine());
         //int n2 = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine(),",");
         int n1 = Integer.parseInt(st.nextToken());
+        int n2 = Integer.parseInt(st.nextToken());
 
         StringBuilder sb = new StringBuilder();
 
         sb.append("처음 fibo>> "+fibonacci2(n1));
+        sb.append(" 다음 fibo>> " + fibonacci2(n2));
 
         System.out.println(sb);
     }
