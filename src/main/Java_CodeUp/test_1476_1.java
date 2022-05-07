13eb523e811e0a4edf8e38548816a4c63037ad02
Jakezo <cakim21@naver.com>
2022-05-07 18:27:14 +0900

    배열문제 solve

--- src/main/Java_CodeUp/test_1476_1.java
+++ src/main/Java_CodeUp/test_1476_1.java
@@ -12,14 +12,16 @@ package Java_CodeUp;
 4 8 12 16 19
 7 11 15 18 20
 
-입력이 n m인 경우의 2차원 배열을 출력해보자.*/
+입력이 n m인 경우의 2차원 배열을 출력해보자.
+00, 10, 01 , 20, 11,03,22,13,04
+*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class test_1476 {
+public class test_1476_1 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(),",");
@@ -30,16 +32,33 @@ public class test_1476 {
         int num2 = Integer.parseInt(st.nextToken());
 
         int[][] arr = new int[num1][num2];
-// 기본 n 차원 배열 출력
+        // 기본 n 차원 배열 출력
+
+        //초기값 설정
+        int number =1;
+        for(int i=0; i<=num1+num2-2; i++){
+            // 열반복
+            for(int j=0; j<num2; j++ ){
+                for(int x=0; x<num1; x++){
+                    if (j+x ==i){
+                        arr[x][j]= number++;
+                        break;
+                    }
+                }
+            }
+        }
+
+
         for (int[] i:arr) {
             for(int j : i){
-                sb.append(j+" ");
+                sb.append(j).append(" ");
             }
             sb.append("\n");
         }
         System.out.print(sb);
 
         br.close();
+
 
     }
 }
