f47be820ad71e355567099192e0b5bb02b3fbe35
Jakezo <cakim21@naver.com>
2022-07-03 14:16:25 +0900

    code up 1673

---
+++ src/main/Java_CodeUp/test_1673_1.java
@@ -0,0 +1,43 @@
+package Java_CodeUp;
+
+/*
+* 입력 Permalink
+1) 세 자연수가 입력된다.
+2) 각 값들은 도어락의 내부 보안키값을 의미한다.
+3) 각 값은 1,000이하의 자연수이다.
+
+출력 Permalink
+가장 가격이 저렴한 만능보안키의 ID값을 출력한다.
+
+예시 Permalink
+입력 : 10 20 30
+
+출력 : 10
+* */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class test_1673_1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+        int num3 = Integer.parseInt(st.nextToken());
+
+        int[] ss = {num1, num2, num3};
+
+        for(int i=1; i<ss.length; i++){
+            if(num1 > ss[i]){
+                num1 = ss[i];
+            }
+        }
+        System.out.println(num1);
+
+    }
+}
