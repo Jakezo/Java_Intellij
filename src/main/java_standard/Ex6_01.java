bc374d60c373c0839031f256e6a53bd7413f2cdb
Jakezo <cakim21@naver.com>
2022-10-17 00:11:12 +0900

    자바정석

---
+++ src/main/java_standard/Ex6_01.java
@@ -0,0 +1,18 @@
+package java_standard;
+
+class Ts{
+    void ssd(){
+
+    }
+}
+
+
+public class Ex6_01 {
+    public static void main(String[] args) {
+
+        Ts ts = new Ts();
+        
+        ts.ssd();
+
+    }
+}
