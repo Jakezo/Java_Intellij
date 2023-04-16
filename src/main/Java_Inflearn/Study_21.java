c3cf457cf6eaf94e51800e819969bf66a8eb7425
jake <cakim21@naver.com>
2023-04-16 14:28:41 +0900

    234

---
+++ src/main/Java_Inflearn/Study_21.java
@@ -0,0 +1,29 @@
+package Java_Inflearn;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Study_21 {
+
+
+    public static void main(String[] args) {
+        
+        Study_21 kb = new Study_21();
+
+        Scanner sc = new Scanner(System.in);
+
+      //  int n =sc.nextInt();
+        //int[] arr = new int[n];
+
+        // for(int i=0; i<n; i++){
+
+        //     arr[i]=sc.nextInt();
+        //     System.out.println(i+"||"+sc.nextInt());  // sc.nextInt 자체가 공백 기준으로 나뉘어짐 
+        // }
+        System.out.println(sc.nextInt()+"#");
+        System.out.println(sc.nextInt());
+
+
+    }
+    
+}
