4735c9d6fe131908db0992773b2becaa11de491b
jake <cakim21@naver.com>
2023-05-18 00:35:18 +0900

    ss

---
+++ src/main/Java_Inflearn/Study_sosu.java
@@ -0,0 +1,29 @@
+package Java_Inflearn;
+
+import java.util.Scanner;
+
+public class Study_sosu {
+
+    public int solution(int n){
+       int [] nums = new int[n+1];
+        int cnt = 0;
+       for(int i=2; i<=n; i++){
+        if(nums[i]==0) cnt++;
+            for(int j=i; j<=n; j=j+i){
+                nums[j]=1;
+            }
+       }
+
+        return cnt;
+    }
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        Study_sosu kb = new Study_sosu();
+
+        int n = sc.nextInt();
+
+        System.out.println(kb.solution(n));
+    }
+    
+}
