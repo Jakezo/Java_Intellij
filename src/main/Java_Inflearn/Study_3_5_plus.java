f29a732b90bb904df3750338ecbd72fb584ed85a
jake <cakim21@naver.com>
2023-11-21 22:10:34 +0900

    study0305 plus

--- src/main/Java_Inflearn/Study_3_5_plus.java
+++ src/main/Java_Inflearn/Study_3_5_plus.java
@@ -6,8 +6,6 @@ public class Study_3_5_plus {
 
     public static void main(String[] args) {
 
-
-
         Scanner sc = new Scanner(System.in);
         Study_3_5_plus T = new Study_3_5_plus();
 
@@ -17,25 +15,14 @@ public class Study_3_5_plus {
     }
 
     private int solution(int n) {
-        int answer=0,sum=0,lt=0;
-        int m = n/2+1;
-        int[] arr = new int[m];
-
-        for (int i = 0; i < m; i++) {
-             arr[i]= i +1;
-        }
-        for (int rt = 0; rt < m; rt++) {
-             sum += arr[rt];
-             if(sum == n){
-                 answer++;
-             }
-
-             while(sum >= n){
-                 sum-= arr[lt++];
-                 if(sum ==n){
-                     answer++;
-                 }
-             }
+        int answer=0,cnt=1;
+        n--;
+        while (n>0){
+            cnt++;
+            n-=cnt;
+            if(n%cnt ==0){
+                answer++;
+            }
         }
         return answer;
     }
---
+++ src/main/Java_Inflearn/Study_3_6.java
@@ -0,0 +1,29 @@
+package Java_Inflearn;
+
+import java.util.Scanner;
+
+public class Study_3_6 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Study_3_6 T = new Study_3_6();
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(T.solution());
+
+    }
+
+    private int solution() {
+        int answer =0,lt=0;
+
+
+        return answer;
+    }
+}
