9e4c6a5a4115d3ea753a5ba8556191f8aae1dc50
jake <cakim21@naver.com>
2024-02-01 00:19:30 +0900

    study0503 solve22

---
+++ src/main/Java_Inflearn/Study_4_3_plus.java
@@ -0,0 +1,48 @@
+package Java_Inflearn;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Study_4_3_plus {
+
+    public static void main(String[] args) {
+
+        Study_4_3_plus T = new Study_4_3_plus();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+             arr[i] = sc.nextInt();
+        }
+
+        for (int x : T.solution(n, k, arr)) {
+            System.out.print(x+" ");
+        }
+    }
+
+    private ArrayList<Integer> solution(int n, int k, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        HashMap<Integer, Integer> hash = new HashMap<>();
+
+        for (int i = 0; i < k-1; i++) {
+            hash.put(arr[i], hash.getOrDefault(arr[i], 0) + 1);
+        }
+        int lt=0;
+        for (int rt = k-1; rt < n; rt++) {
+            hash.put(arr[rt], hash.getOrDefault(arr[rt],0)+1);
+            answer.add(hash.size());
+            hash.put(arr[lt], hash.get(arr[lt]) - 1);
+            if (hash.get(arr[lt]) == 0) {
+                hash.remove(arr[lt]);
+            }
+            lt++;
+
+        }
+        return answer;
+    }
+}
